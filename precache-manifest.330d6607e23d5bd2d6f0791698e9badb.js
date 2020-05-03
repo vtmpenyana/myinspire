8b8d82d9e84cec6ef222b43df3659986c6288c11
Vincent-Mpenyana <vincent.mpenyana8@gmail.com>
2020-05-03 15:25:10 +0200

    Updates

---
+++ precache-manifest.330d6607e23d5bd2d6f0791698e9badb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ed5f52dd1066e1a0f580e09b005fe36",
+    "url": "/myinspire/index.html"
+  },
+  {
+    "revision": "9c347466107ba443fdaf",
+    "url": "/myinspire/static/css/2.8fe626b3.chunk.css"
+  },
+  {
+    "revision": "52f75f955ce62c670ebc",
+    "url": "/myinspire/static/css/main.00cc73cb.chunk.css"
+  },
+  {
+    "revision": "9c347466107ba443fdaf",
+    "url": "/myinspire/static/js/2.776d1a41.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myinspire/static/js/2.776d1a41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52f75f955ce62c670ebc",
+    "url": "/myinspire/static/js/main.b690f9db.chunk.js"
+  },
+  {
+    "revision": "e6fa509f58e1bc961b92",
+    "url": "/myinspire/static/js/runtime-main.04f177c0.js"
+  }
+]);
